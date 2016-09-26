1fab7d2b62bda880d8c596bfec352fa513072e9b
Jim Teunis <jim.teunis@gmail.com>
2016-09-26 17:40:03 +0200

    Added Mutator interface and MutateAddGaussian example class

---
+++ src/MutateAddGaussian.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+
+public class MutateAddGaussian implements Mutator {
+	
+	Random rand = new Random();
+	double scale;
+	
+	public MutateAddGaussian(double scale){
+		this.scale = scale;
+	}
+
+	public Tuple mutate(Tuple tomutate) {
+		double[] oldvector = tomutate.vector;
+		double[] newvector = new double[oldvector.length];
+		for(int i = 0; i < oldvector.length; i++){
+			newvector[i] = oldvector[i] + scale*rand.nextGaussian();
+		}
+		return new Tuple(newvector);
+	}
+	
+	
+
+}
